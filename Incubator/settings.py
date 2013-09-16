a9f8d16cab0c4786414bae68b130c0c55f6fcf9d
gmartinvela <gustavo.martin@opendeusto.es>
2013-09-16 15:10:40 +0200

    Fixed problems to include the login

--- Incubator/settings.py
+++ Incubator/settings.py
@@ -9,15 +9,17 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
+LOGIN_REDIRECT_URL = 'home'
+
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'Incubator',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'root',
+        'PASSWORD': 'letmein',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
--- Incubator/urls.py
+++ Incubator/urls.py
@@ -8,9 +8,12 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'Incubator.views.home', name='home'),
     url(r'^measures$', 'Incubator.views.measures', name='measures'),
-    url(r'^lights/(?P<light_number>\w+)/(?P<command>\w+)/$', 'Incubator.views.lights', name='lights')
+    url(r'^lights/(?P<light_number>\w+)/(?P<command>\w+)/$', 'Incubator.views.lights', name='lights'),
     # url(r'^Incubator/', include('Incubator.foo.urls')),
-
+    url(r'^login/', 'django.contrib.auth.views.login', name='login'),
+    #url(r'^accounts/logout/','django.contrib.auth.views.logout', {'template_name': 'cuentas_X/logged_out_bootstrapped.html'}),
+    url(r'^logout/', 'django.contrib.auth.views.logout', name='logout'),
+	#url(r'^(?P<nombre>[\w|\W]+)/$', views.CV, name='CV'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
