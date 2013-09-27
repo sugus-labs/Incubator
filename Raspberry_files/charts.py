8ceb9ec17c1dedb698f0c7bfcb49eb4e496a86f5
gmartinvela <gustavo.martin@opendeusto.es>
2013-09-27 16:43:46 +0200

    All the functions changed to works with Pandas libarry

--- Raspberry_files/charts.py
+++ Raspberry_files/charts.py
@@ -13,8 +13,8 @@ import pandas.io.sql as psql
 MAD=pytz.timezone('Europe/Madrid')
 datetime_format = '%Y-%m-%d %H:%M:%S'
 
-temp_param_MAX = 37.3
-temp_param_MIN = 37.2
+temp_param_MAX = 37.5
+temp_param_MIN = 37.4
 temp_MIN = 37.0
 temp_MAX = 38.0
 humi_param_MAX = 60.5
@@ -75,110 +75,83 @@ def extract_data_from_DB(datetime_format, db_local_path, db_utils_dict):
 	    dataframe_sqlite_MAD = dataframe_sqlite_UTC.tz_convert(MAD)
 	return dataframe_sqlite_MAD
 
-def save_humi_from_dataframe_by_day(dataframe, string_day):
-	plt.title('Humidity of day  %s' % string_day)
+def save_humi_from_dataframe_by_day(dataframe, string_day, humi_param_MAX, humi_param_MIN, humi_MAX, humi_MIN, humi_limit_SUP, humi_limit_INF):
+	plt.title('Humidity of day %s' % string_day)
 	today_plot = dataframe.humi[string_day]
-	today_plot.tail(100).plot()
-	#plt.savefig('humi_%s.png' % string_day, orientation='landscape')
-	plt.show()
+	plt.ylim(humi_limit_INF, humi_limit_SUP)
+	today_plot.plot()
+	plt.axhspan(humi_param_MIN, humi_param_MAX, facecolor='g', alpha=0.2)
+	plt.axhspan(humi_MIN, humi_MAX, facecolor='g', alpha=0.2)
+ 	today_plot_mean = today_plot.mean()
+ 	legend( ('MEAN: %.2f' % today_plot_mean, 'Recommended zone') , loc = 'upper right')
+ 	plt.ylabel('Humidity')
+	plt.xlabel('Hours')
+	plt.savefig('humi_%s.png' % string_day, orientation='landscape')
 	plt.close()
 
 def save_temp_from_dataframe_by_day(dataframe, string_day, temp_param_MAX, temp_param_MIN, temp_MAX, temp_MIN, temp_limit_SUP, temp_limit_INF):
-	plt.title('Temperature of day  %s' % string_day)
+	plt.title('Temperature of day %s' % string_day)
 	today_plot = dataframe.TEMP_LOG[string_day]
 	plt.ylim(temp_limit_INF, temp_limit_SUP)
  	y=np.arange(temp_limit_INF, temp_limit_SUP, 0.2)
+ 	plt.grid()
  	plt.yticks(y)
-	today_plot.tail(600).plot()
-	#today_plot_mean = today_plot.tail(100).mean()
-	today_plot_last = today_plot.tail(1).mean()
-	legend( (today_plot_last, 'SHT1x') , loc = 'upper left')
-	#print "MEAN: ", today_plot_mean
+	today_plot.plot()
+	plt.axhspan(temp_param_MIN, temp_param_MAX, facecolor='g', alpha=0.2)
+	plt.axhspan(temp_MIN, temp_MAX, facecolor='g', alpha=0.2)
+	today_plot_mean = today_plot.mean()
+	legend( ('MEAN: %.2f' % today_plot_mean, 'Recommended zone') , loc = 'upper right')
 	plt.savefig('temp_%s.png' % string_day, orientation='landscape')
-	#plt.show()
 	plt.close()
 
-# def generate_comparing_temperature_with_dates_and_save(thermo_dates, thermo_temps, SHT1x_dates, SHT1x_temps, temp_param_MAX, temp_param_MIN, temp_MAX, temp_MIN, temp_limit_SUP, temp_limit_INF):
-# 	plt.plot(thermo_dates, thermo_temps, 'r', SHT1x_dates, SHT1x_temps,)
-# 	plt.ylim(temp_limit_INF, temp_limit_SUP)
-# 	y=np.arange(temp_limit_INF, temp_limit_SUP, 0.5)
-# 	plt.grid()
-# 	plt.gcf().autofmt_xdate()
-# 	legend( ('thermo', 'SHT1x') ,
-#         loc = 'upper right')
-# 	plt.savefig('thermo_comparation.png', orientation='landscape')
-# 	#plt.show()
-# 	plt.close()
+def comparing_temps_from_dataframe_by_day(dataframe_thermo, dataframe_SHT1x, string_day, temp_param_MAX, temp_param_MIN, temp_MAX, temp_MIN, temp_limit_SUP, temp_limit_INF):
+	plt.title('Comparation of the temperatures of day %s' % string_day)
+	today_plot_thermo = dataframe_thermo.TEMP_LOG[string_day]
+	today_plot_SHT1x = dataframe_SHT1x.temp[string_day]
+	plt.ylim(temp_limit_INF, temp_limit_SUP)
+ 	y=np.arange(temp_limit_INF, temp_limit_SUP, 0.2)
+ 	plt.grid()
+ 	plt.yticks(y)
+	today_plot_thermo.plot()
+	today_plot_SHT1x.plot()
+	plt.axhspan(temp_param_MIN, temp_param_MAX, facecolor='g', alpha=0.2)
+	plt.axhspan(temp_MIN, temp_MAX, facecolor='g', alpha=0.2)
+	legend( ('thermo', 'SHT1x', 'Recommended zone') , loc = 'upper right')
+	plt.savefig('comparing_temps_%s.png' % string_day, orientation='landscape')
+	plt.close()
 
-# def generate_thermo_plot_and_save(thermo_dates, thermo_temps, thermo_status, temp_param_MAX, temp_param_MIN, temp_MAX, temp_MIN, temp_limit_SUP, temp_limit_INF):
-# 	plt.title('Data from thermometer')
-# 	# First subplot
-# 	# plt.subplot(2, 1, 1)
-# 	# yticks = ['BAD', 'GOOD']
-# 	# y=[0,1]
-# 	# plt.yticks(y,yticks)
-# 	# plt.plot(thermo_dates,thermo_status)
-# 	# plt.ylim(-0.1, 1.1)
-# 	# Second subplot
-# 	#plt.subplot(2, 1, 2)
-# 	plot_date(thermo_dates, thermo_temps, 'b-', tz=MAD)
-# 	plt.ylim(temp_limit_INF, temp_limit_SUP)
-# 	y=np.arange(temp_limit_INF, temp_limit_SUP, 0.5)
-# 	plt.grid()
-# 	plt.yticks(y)
-# 	plt.axhspan(temp_MIN, temp_MAX, facecolor='r', alpha=0.5)
-# 	plt.axhspan(temp_param_MIN, temp_param_MAX, facecolor='m', alpha=0.2)
-# 	plt.gcf().autofmt_xdate()
-# 	plt.xlabel('time')
-# 	plt.ylabel('Temperature')
-# 	plt.savefig('thermo.png', orientation='landscape')
-# 	#plt.show()
-# 	plt.close()
+def comparing_humi_temps_from_dataframe_by_day(dataframe_thermo, dataframe_SHT1x, string_day, temp_param_MAX, temp_param_MIN, temp_MAX, temp_MIN, temp_limit_SUP, temp_limit_INF, humi_limit_SUP, humi_limit_INF):
+	plt.title('Correlation between temperature and humidity')
+ 	# First subplot
+ 	plt.subplot(2, 1, 1)
+	today_plot_thermo = dataframe_thermo.TEMP_LOG[string_day]
+ 	plt.ylim(temp_limit_INF, temp_limit_SUP)
+ 	y=np.arange(temp_limit_INF, temp_limit_SUP, 0.2)
+ 	plt.grid()
+ 	plt.yticks(y)
+	today_plot_thermo.plot()
+	plt.axhspan(temp_param_MIN, temp_param_MAX, facecolor='g', alpha=0.2)
+	plt.axhspan(temp_MIN, temp_MAX, facecolor='g', alpha=0.2)
+	# Second subplot
+ 	plt.subplot(2, 1, 2)
+	today_plot_SHT1x = dataframe_SHT1x.humi[string_day]
+	plt.ylim(humi_limit_INF, humi_limit_SUP)
+ 	plt.grid()
+	today_plot_SHT1x.plot()
+	plt.axhspan(humi_param_MIN, humi_param_MAX, facecolor='g', alpha=0.2)
+	plt.axhspan(humi_MIN, humi_MAX, facecolor='g', alpha=0.2)
+	#legend( ('thermo', 'SHT1x', 'Recommended zone') , loc = 'upper right')
+	plt.savefig('comparing_humi_temps_%s.png' % string_day, orientation='landscape')
+	plt.close()
 
-# def generate_SHT1x_plot_and_save(SHT1x_dates, SHT1x_temps, SHT1x_humis, humi_param_MAX, humi_param_MIN, humi_MAX, humi_MIN, temp_limit_SUP, temp_limit_INF):
-# 	plt.title('Data from SHT1x')
-# 	# First subplot
-# 	plt.subplot(2, 1, 1)
-# 	plot_date(SHT1x_dates, SHT1x_humis, 'b-', tz=MAD)
-# 	plt.axhspan(humi_MIN, humi_MAX, facecolor='r', alpha=0.5)
-# 	plt.axhspan(humi_param_MIN, humi_param_MAX, facecolor='m', alpha=0.2)
-# 	plt.ylabel('Humidity')
-# 	# Second subplot
-# 	plt.subplot(2, 1, 2)
-# 	plot_date(SHT1x_dates, SHT1x_temps, 'b-', tz=MAD)
-# 	plt.ylim(temp_limit_INF, temp_limit_SUP)
-# 	y=np.arange(temp_limit_INF, temp_limit_SUP, 0.5)
-# 	plt.grid()
-# 	plt.axhspan(temp_MIN, temp_MAX, facecolor='r', alpha=0.5)
-# 	plt.axhspan(temp_param_MIN, temp_param_MAX, facecolor='m', alpha=0.2)
-# 	plt.gcf().autofmt_xdate()
-# 	plt.xlabel('time')
-# 	plt.ylabel('Temperature')	
-# 	plt.savefig('SHT1x.png', orientation='landscape')
-# 	#plt.show()
-# 	plt.close()
-
-# retrieve_DBs()
-# thermo_dates, thermo_temps, thermo_status = extract_thermo_data(datetime_format, local_path_thermodb, thermodb_utils_dict)
-# SHT1x_dates, SHT1x_temps, SHT1x_humis = extract_SHT1x_data(datetime_format, local_path_SHT1xdb, SHT1xdb_utils_dict)
-# generate_comparing_temperature_with_dates_and_save(thermo_dates, thermo_temps, SHT1x_dates, SHT1x_temps, temp_param_MAX, temp_param_MIN, temp_MAX, temp_param_MIN, 40, 35)
-# generate_thermo_plot_and_save(thermo_dates, thermo_temps, thermo_status, temp_param_MAX, temp_param_MIN, temp_MAX, temp_MIN, 40, 35)
-# generate_SHT1x_plot_and_save(SHT1x_dates, SHT1x_temps, SHT1x_humis, humi_param_MAX, humi_param_MIN, humi_MAX, humi_MIN, 40, 35)
-
-# def retrieve_DB():
-# 	with sqlite3.connect('/home/weblord/Desktop/Incubator/Raspberry_files/SHT1x/SHT1x.db', detect_types=sqlite3.PARSE_DECLTYPES) as conn:
-# 	    df_sqlite = psql.frame_query('select * from READ', con=conn)    
-# 	    print 'loaded dataframe from sqlite', len(df_sqlite)
-# 	    df_sqlite.index = pd.to_datetime(df_sqlite.pop('date'))
-# 	    today_data =  df_sqlite['2013-09-26']
-# 	    today_data.plot()
-# 	    plt.show()
 
 retrieve_DBs()
 SHT1x_dataframe = extract_data_from_DB(datetime_format, local_path_SHT1xdb, SHT1xdb_utils_dict)
 thermo_dataframe = extract_data_from_DB(datetime_format, local_path_thermodb, thermodb_utils_dict)
-save_humi_from_dataframe_by_day(SHT1x_dataframe, '2013-09-27')
-save_temp_from_dataframe_by_day(thermo_dataframe, '2013-09-27', temp_param_MAX, temp_param_MIN, temp_MAX, temp_param_MIN, 41, 36)
+save_humi_from_dataframe_by_day(SHT1x_dataframe, '2013-09-27', humi_param_MAX, humi_param_MIN, humi_MAX, humi_MIN, 75, 50)
+save_temp_from_dataframe_by_day(thermo_dataframe, '2013-09-27', temp_param_MAX, temp_param_MIN, temp_MAX, temp_MIN, 39, 36.8)
+comparing_temps_from_dataframe_by_day(thermo_dataframe, SHT1x_dataframe, '2013-09-27', temp_param_MAX, temp_param_MIN, temp_MAX, temp_MIN, 39, 36.8)
+comparing_humi_temps_from_dataframe_by_day(thermo_dataframe, SHT1x_dataframe, '2013-09-27', temp_param_MAX, temp_param_MIN, temp_MAX, temp_MIN, 39, 36.8, 75, 50)
 #print SHT1x_dataframe
 #print thermo_dataframe
 
