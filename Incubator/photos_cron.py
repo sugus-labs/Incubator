134e082448788d146bc39a42fde97c4728a06173
gmartinvela <gustavo.martin@opendeusto.es>
2013-10-10 18:17:38 +0200

    Added function to take images every hour

--- Incubator/photos_cron.py
+++ Incubator/photos_cron.py
@@ -3,11 +3,9 @@
 from crontab import CronTab
 import sys
 
-cron = CronTab(tab='')
+cron = CronTab()
 job = cron.new(command='python /home/gustavo/Desktop/Incubator/Incubator/take_photos.py', comment='Incubator cron')
 job.hour.every(1)
-
-sys.stdout.write(str(cron.render()))
-cron.write()
+job.enable()
 
 print unicode(cron)
--- Incubator/take_photos.py
+++ Incubator/take_photos.py
@@ -10,26 +10,26 @@ URL_cam_3 = 'http://192.168.0.152/image.jpg'
 URL_cams = [URL_cam_1, URL_cam_2, URL_cam_3]
 
 def retrieve_images():
-	on_request = urllib2.urlopen('http://127.0.0.1/lights/ALL/ON')
+	proxy_handler = urllib2.ProxyHandler({})
+	opener = urllib2.build_opener(proxy_handler)
+	urllib2.install_opener(opener)
+	on_request = urllib2.urlopen('http://localhost:8008/lights/ALL/ON')
 	on_response = on_request.read()
 	if on_response == '200 OK':
 		time.sleep(2)
 		localtime = time.localtime()
 		time_now = time.strftime("%Y%m%d%H%M%S", localtime)
-		try:
-			for cam_num, cam in enumerate(URL_cams):
-				proxy_handler = urllib2.ProxyHandler({})
-				opener = urllib2.build_opener(proxy_handler)
-				urllib2.install_opener(opener)
-				image_path = 'Incubator/static/egg_programmed_images/image_%s_%s.jpg' % (int(cam_num) + 1, time_now)
-				with open(image_path,'wb') as f:
-					f.write(urllib2.urlopen(cam.read())
-					f.close()
-			#im = cv2.imread("image.jpg")
-		except:
+		for cam_num, cam in enumerate(URL_cams):
+			#print cam
+			proxy_handler = urllib2.ProxyHandler({})
+			opener = urllib2.build_opener(proxy_handler)
+			urllib2.install_opener(opener)
+			image_path = 'static/egg_programmed_images/image_%s_%s.jpg' % (str(int(cam_num) + 1), time_now)
+			with open(image_path,'wb') as f:
+				f.write(urllib2.urlopen(cam).read())
+				f.close()
+		except
 			print "No connection!"
-		off_request = urllib2.urlopen('http://127.0.0.1/lights/ALL/ON')
-		#off_response = off_request.read()
-		#while off_response != '200 O'
+	off_request = urllib2.urlopen('http://localhost:8008/lights/ALL/OFF')
 
 retrieve_images()
